0af0bea73794146097aae0346f94132cbbfc765c
J Lomako <jlomako@web.de>
2023-03-22 09:17:36 -0400

    add function create_spyro

--- main.py
+++ main.py
@@ -16,22 +16,27 @@ cmap = mpl.cm.summer
 plt.style.use('dark_background')
 # #plt.style.use('default')
 
-num_spirographs = np.random.randint(2, 4)
+@st.cache_data
+def create_spyro(number):
+    num_spirographs = np.random.randint(2, 4)
+    fig = plt.figure()
+    for i in range(num_spirographs):
+        R = np.random.randint(1, 99)  # big circle radius
+        r = np.random.randint(1, 50)  # small circle radius
+        d = np.random.randint(1, 50)  # distance from the center of the small circle to the tracing point
+        theta = np.linspace(0, 50 * np.pi, 500)
+        x = (R - r) * np.cos(theta) + d * np.cos((R - r) / r * theta)
+        y = (R - r) * np.sin(theta) - d * np.sin((R - r) / r * theta)
+        plt.plot(x, y, c=cmap(i / num_spirographs), linewidth=0.3)
+    plt.axis('equal')
+    plt.axis('off')
+    st.pyplot(fig)
 
-fig = plt.figure()
-for i in range(num_spirographs):
-    R = np.random.randint(1, 99)  # big circle radius
-    r = np.random.randint(1, 50)  # small circle radius
-    d = np.random.randint(1, 50)  # distance from the center of the small circle to the tracing point
-    theta = np.linspace(0, 50 * np.pi, 500)
-    x = (R - r) * np.cos(theta) + d * np.cos((R - r) / r * theta)
-    y = (R - r) * np.sin(theta) - d * np.sin((R - r) / r * theta)
-    #plt.plot(x, y, linewidth=0.3)
-    plt.plot(x, y, c=cmap(i / num_spirographs), linewidth=0.3)
 
-plt.axis('equal')
-plt.axis('off')
+if st.button("Create"):
+    create_spyro(np.random.randint(1, 9999)) # refresh cache with random numbers
 
-st.pyplot(fig)
+if st.button("Download"):
+    None
 
-st.button("Rerun")
+
