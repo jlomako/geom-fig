f88395466233807554d138fed1d06f77c14a55ac
J Lomako <jlomako@web.de>
2023-03-21 22:12:50 -0400

    adding colorpalette

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 st.title("Spirograph")
 
+cmap = mpl.cm.summer
+plt.style.use('dark_background')
+#plt.style.use('default')
+
 num_spirographs = np.random.randint(2, 4)
 
+fig = plt.figure()
 for i in range(num_spirographs):
     R = np.random.randint(1, 99)  # big circle radius
     r = np.random.randint(1, 50)  # small circle radius
@@ -14,11 +20,10 @@ for i in range(num_spirographs):
     theta = np.linspace(0, 50 * np.pi, 500)
     x = (R - r) * np.cos(theta) + d * np.cos((R - r) / r * theta)
     y = (R - r) * np.sin(theta) - d * np.sin((R - r) / r * theta)
-    plt.plot(x, y, linewidth=0.3)
+    #plt.plot(x, y, linewidth=0.3)
+    plt.plot(x, y, c=cmap(i / num_spirographs), linewidth=0.3)
 
 plt.axis('equal')
 plt.axis('off')
 
-fig = plt
 st.pyplot(fig)
-
